9144d8fe1a38c011a7cf62159019aa26d2023195
jeff2857 <jeffliu2857@gmail.com>
2024-08-05 11:51:14 +0800

    test

---
+++ app/Connect.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { useSDK } from "@metamask/sdk-react";
+import React, { useState } from "react";
+
+export const Connect = () => {
+  const [account, setAccount] = useState<string>();
+  const { sdk, connected, connecting, provider, chainId } = useSDK();
+
+  const connect = async () => {
+    try {
+      const accounts = await sdk?.connect();
+      setAccount(accounts?.[0]);
+    } catch (err) {
+      console.warn("failed to connect..", err);
+    }
+  };
+
+  return (
+    <div className="App">
+      <button style={{ padding: 10, margin: 10 }} onClick={connect}>
+        Connect
+      </button>
+      {connected && (
+        <div>
+          <>
+            {chainId && "Connected chain: ${chainId}"}
+            <p></p>
+            {account && "Connected account: ${account}"}
+          </>
+        </div>
+      )}
+    </div>
+  );
+};
